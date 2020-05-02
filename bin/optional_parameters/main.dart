38e6644575314f8509b654358c04636202a8bd90
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-01 20:45:14 -0400

    Use the constructor

--- bin/optional_parameters/main.dart
+++ bin/optional_parameters/main.dart
@@ -1,3 +1,10 @@
-void main() {
+import 'dart:math';
 
+import 'rectangle.dart';
+
+void main() {
+  print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
+  print(Rectangle(origin: const Point(10, 10)));
+  print(Rectangle(width: 200));
+  print(Rectangle());
 }

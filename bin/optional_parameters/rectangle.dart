a4fbda3080142eed6b69646c18c194eb169e513c
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-01 20:42:09 -0400

    Improve the output

--- bin/optional_parameters/rectangle.dart
+++ bin/optional_parameters/rectangle.dart
@@ -6,4 +6,7 @@ class Rectangle {
   Point origin;
 
   Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
+
+  @override
+  String toString() => 'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
 }

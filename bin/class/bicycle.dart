7f171c9109aac788ec310a200ce7305aa361b8f9
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-01 20:29:30 -0400

    Add a read-only variable

--- bin/class/bicycle.dart
+++ bin/class/bicycle.dart
@@ -1,10 +1,19 @@
 class Bicycle {
   int cadence;
-  int speed;
+  int _speed = 0;
+  int get speed => _speed;
   int gear;
 
-  Bicycle(this.cadence, this.speed, this.gear);
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
 
   @override
-  String toString() => 'Bicycle: $speed mph';
+  String toString() => 'Bicycle: $_speed mph';
 }
--- bin/class/main.dart
+++ bin/class/main.dart
@@ -1,6 +1,6 @@
 import 'bicycle.dart';
 
 void main() {
-  var bike = Bicycle(2, 0, 1);
+  var bike = Bicycle(2, 1);
   print(bike);
 }

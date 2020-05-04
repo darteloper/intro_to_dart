8b4990e9ae37c4c8a9dffe673e0da450132eee56
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-04 00:13:37 -0400

    Convert imperative code to functional

--- bin/functional/main.dart
+++ bin/functional/main.dart
@@ -2,7 +2,5 @@ String scream(int length) => "A${'a' * length}h!";
 
 void main() {
   final values = [1, 2, 3, 5, 10, 50];
-  for (var length in values) {
-    print(scream(length));
-  }
+  values.map(scream).forEach(print);
 }

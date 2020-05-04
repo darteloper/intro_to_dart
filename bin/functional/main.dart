93706de86de2a43c39b402225075f287a9164eda
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-04 00:14:59 -0400

    Use more Iterable features

--- bin/functional/main.dart
+++ bin/functional/main.dart
@@ -1,6 +1,6 @@
 String scream(int length) => "A${'a' * length}h!";
 
-void main() {
+main() {
   final values = [1, 2, 3, 5, 10, 50];
-  values.map(scream).forEach(print);
+  values.skip(1).take(3).map(scream).forEach(print);
 }

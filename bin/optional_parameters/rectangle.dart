c1f41c15b94d242f5e602a201965cb084adfa004
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-11-17 12:41:23 -0400

    Format the optional_parameters code directory

--- bin/optional_parameters/rectangle.dart
+++ bin/optional_parameters/rectangle.dart
@@ -8,5 +8,6 @@ class Rectangle {
   Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
 
   @override
-  String toString() => 'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
+  String toString() =>
+      'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
 }
